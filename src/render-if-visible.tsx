4e4917de5164b8443607104f7c0d55e874465691
koji98 <34973870+koji98@users.noreply.github.com>
2021-12-17 19:34:15 -0800

    Update render-if-visible.tsx

--- src/render-if-visible.tsx
+++ src/render-if-visible.tsx
@@ -1,24 +1,39 @@
-import React, { useState, useRef, useEffect } from 'react'
+/********************************************************************************
+ *** Description: Altered source of RenderedIfVisible NPM package due to stutter
+ ***              effect when directly using the package.
+ ***
+ *** https://dev.to/angus_russell/super-simple-list-virtualization-in-react-with-intersectionobserver-3o6g
+ *** https://github.com/NightCafeStudio/react-render-if-visible/blob/main/src/render-if-visible.tsx
+ ********************************************************************************/
+
+import { useState, useRef, useEffect } from 'react';
+import type { ReactNode } from 'react';
+import { Dictionary } from '../types/common';
 
 const isServer = typeof window === 'undefined'
 
 type Props = {
-  /** An estimate of the element's height */
+  background?: string,
+  innerStyle?: Dictionary,
+  /** An estimate of the element's height/width */
   defaultHeight?: number
+  defaultWidth?: number,
   /** How far outside the viewport in pixels should elements be considered visible?  */
   visibleOffset?: number
   root?: HTMLElement | null
-  children: React.ReactNode
+  children: ReactNode
 }
 
 const RenderIfVisible = ({
+  background = 'var(--color-bg)',
+  innerStyle = null,
   defaultHeight = 300,
+  defaultWidth = 300,
   visibleOffset = 1000,
   root = null,
   children
 }: Props) => {
   const [isVisible, setIsVisible] = useState<boolean>(isServer)
-  const placeholderHeight = useRef<number>(defaultHeight)
   const intersectionRef = useRef<HTMLDivElement>(null)
 
   // Set visibility with intersection observer
@@ -46,22 +61,21 @@ const RenderIfVisible = ({
         }
       }
     }
-    return () => {}
+    return () => { }
   }, [intersectionRef])
-
-  // Set true height for placeholder element after render.
-  useEffect(() => {
-    if (intersectionRef.current && isVisible) {
-      placeholderHeight.current = intersectionRef.current.offsetHeight
-    }
-  }, [isVisible, intersectionRef])
 
   return (
     <div ref={intersectionRef}>
       {isVisible ? (
         <>{children}</>
       ) : (
-        <div style={{ height: placeholderHeight.current }} />
+        innerStyle ? (
+          <div style={{ height: `${defaultHeight}px`, width: `${defaultWidth}px` }}>
+            <div style={innerStyle} />
+          </div>
+        ) : (
+          <div style={{ height: `${defaultHeight}px`, width: `${defaultWidth}px`, background: background }} />
+        )
       )}
     </div>
   )
